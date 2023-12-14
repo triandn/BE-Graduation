2cb73675acd03e17e90b58b25eeb7001fc172872
triandn <triandn43@gmail.com>
2023-12-14 17:17:50 +0700

    update create api

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 var cors = require('cors')
 const app = express();
 app.use(express.json());
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 
 const dotenv = require('dotenv');
 dotenv.config();
 
 const itemsPool = require('./DBConfig')
+const comments = [];
 
 app.get('/', (req, res) => {
     res.send('API homepage');
@@ -28,10 +31,14 @@ app.get('/api/users', async(req, res) => {
 
 app.post('/api/users', async (req, res) => {
     const { name, email, content } = req.body;
+    comments.push({ name, content, email });
+    name_insert = name;
+    email_insert = email;
+    content_insert = content;
     try {
         const newItem = await itemsPool.query(
             'INSERT INTO users (name, email, content) VALUES ($1, $2, $3) RETURNING *',
-            [name, email, content]
+            [name_insert, email_insert, content_insert]
         );
         res.status(201).json({ 
             message: "New item added!",
